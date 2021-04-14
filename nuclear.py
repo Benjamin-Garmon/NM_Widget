6d2110a962729ade2af6819ed3013b8d30cca47a
Benjamin Garmon <73196056+InfirtleSpring@users.noreply.github.com>
2021-04-13 22:03:14 -0400

    Update nuclear.py
    fixed the math. minutes should have been subtracted, made hours a decimal for a more accurate calculation

--- nuclear.py
+++ nuclear.py
@@ -14,20 +14,11 @@ def concentration(mCinow, mCi):
 	return round(mLs,2)
 
 
-if now.minute <= 20:
-	minutes = 0
-elif 21 <= now.minute <= 39:
-	minutes = 0.5
-else:
-	minutes = 1
-
-timerange = 24 - now.hour + 7 + minutes
-syringeactivity = int(input('how many mCi you have in syringe: '))
-
-#converting mCi to uCi
-thismany = int(mCineeded(timerange)*1000)
+#24 hr per day - how many hours have passed so far = hr till midnight. then add 7 to get to 7 a.m. minus the minutes in the hour so far, rounded to 2 decimal places
+timerange = round(24 - now.hour + 7 - (now.minute/60),2)
+syringeactivity = float(input('how many mCi you have in syringe: '))
 		
 #QC with saline to reach the needed concentration for 1 drop to = mCi needed
 QCto = concentration(int(syringeactivity),mCineeded(timerange))
-		
+
 print(f'There are {timerange} hours between now and 7 a.m. tomorrow \n You will need to QC your dose to {QCto} mLs')
